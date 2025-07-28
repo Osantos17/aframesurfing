7a6643ee461a61c09ed1de7c92e8610f6dd634cc
Orlando Santos <Santos.Orlando17@gmail.com>
2025-07-28 17:23:30 +1000

    add browser router 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 import App from './App.jsx'
@@ -8,11 +8,11 @@ import Download from './Download.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/download" element={<Download />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 )
